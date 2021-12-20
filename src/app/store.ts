72385e5ccba1e46656d8858d85759217f9d4aac5
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-12-20 18:46:04 +0500

    initial project setup, connecting the redux-saga to the redux-toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,23 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import {
+  configureStore,
+  combineReducers,
+  ThunkAction,
+  Action
+} from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
 import counterReducer from '../features/counter/counterSlice';
 
+const sagaMiddleware = createSagaMiddleware();
+
+const reducer = combineReducers({
+  counter: counterReducer,
+});
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ 
+    // thunk: false 
+  }).concat(sagaMiddleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
